911c6238bdf391a2d85791466e90e77105487f08
Red Daly <reddaly@gmail.com>
2009-11-30 19:58:07 -0800

    new exports and STOP-EVENT

--- src/packages.lisp
+++ src/packages.lisp
@@ -3,13 +3,16 @@
   (:export
    ;; parenscript symbols
    #:event
+   #:event-emitter
    #:event-origin
+   #:event-cancelled?
    #:event-description
    #:event-handlers
    #:event-fired
    #:register-event-handler
    #:register-dom-event-handler
    #:event-handler-fits?
+   #:stop-event
    #:stop-dom-event))
 
 
